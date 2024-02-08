9b21f5c7a4662965c6378d343e3d5f9bc6722eb6
Hiago Braga <hiagoaugustobraga@gmail.com>
2024-02-08 17:23:41 -0300

    Subindo Script do Quiz.

---
+++ scriptQuiz.js
@@ -0,0 +1,42 @@
+const quiz = [
+    {
+      pergunta: "Qual foi a data de lançamento da Netflix?", //andrey
+      opcao: [
+        "30 de Maio de 2000",
+        "7 de Maio de 1994",
+        "29 de Agosto de 1997",
+        "18 de Julho de 2002",
+      ],
+      resposta: "C",
+    },
+    {
+      pergunta: "Que ano nasci", //beatriz
+      opcao: [2005, 2006, 2007, 2008],
+      resposta: "A",
+    },
+    {
+      pergunta: "Qual atributo HTML é usado para definir o texto alternativo para uma imagem?", //brenda
+      opcao: [
+        "alt",
+        "title",
+        "desc",
+        "caption",
+      ],
+      resposta: "A",
+    },
+    {
+      pergunta: "Nome da minha mãe", //hiago
+      opcao: ["tereza", "creuza", "maria", "vitoria"],
+      resposta: "C",
+    },
+    {
+      pergunta: "Qual foi a data e para onde foi o local do primeiro pouso fora de órbita?",
+      opcao: [
+        "20 de julho de 1969 na Lua",
+        "12 de abril de 1961 em Marte",
+        "20 de julho de 1967 na Lua",
+        "31 de agosto de 1963 em Saturno",
+     ],
+        resposta: "A",
+    },
+  ];
